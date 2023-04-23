6d233e91d57a5b0cf744eee60d3705124be5b7e8
Егор <goga.andr.gena@mail.ru>
2023-04-23 13:29:08 +0300

    FormRooms code

--- HostelProduct/FormRoom.cs
+++ HostelProduct/FormRoom.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Controllers;
+using Models.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,183 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Unity;
 
 namespace HostelProduct
 {
     public partial class FormRoom : Form
     {
-        public FormRoom()
+        [Dependency]
+        public new IUnityContainer Container { get; set; }
+
+        private int id;
+        private readonly RoomsController rooms;
+
+        public FormRoom(RoomsController rooms)
         {
             InitializeComponent();
+            this.rooms = rooms;
+        }
+
+        private void FormService_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                List<Room> list = rooms.GetList();
+                if (list != null)
+                {
+                    dataGridView1.DataSource = list;
+                    dataGridView1.Columns[0].Visible = false;
+                    dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                    dataGridView1.Columns[1].HeaderText = "Название комнаты";
+                    dataGridView1.Columns[3].HeaderText = "Стоимость ";
+                    dataGridView1.Columns[4].HeaderText = "Расположение";
+                    dataGridView1.Columns[5].HeaderText = "Кол комнат";
+                    dataGridView1.Columns[6].HeaderText = "Площадь комнаты";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxNameRoom.Text))
+            {
+                MessageBox.Show("Заполните название комнаты!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(textBoxCost.Text))
+            {
+                MessageBox.Show("Напишите стоимость", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(textBoxLocation.Text))
+            {
+                MessageBox.Show("Напишите Адресс", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(textBoxRooms.Text))
+            {
+                MessageBox.Show("Напишите стоимость", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(textBoxSize.Text))
+            {
+                MessageBox.Show("Напишите площадь", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                rooms.AddElement(new Room
+                {
+                    Name = textBoxNameRoom.Text,
+                    Price = Convert.ToDouble(textBoxCost.Text),
+                    Location=TextBoxLocation.Text,
+                    Rooms=Convert.ToInt32(TextBoxRooms.Text),
+                    Size=Convert.ToDouble(TextBoxSize.Text)
+                });
+                MessageBox.Show("Услуга успешно добавлена!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Введите корректные значения", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            LoadData();
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxNameRoom.Text))
+            {
+                MessageBox.Show("Заполните название комнаты!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(textBoxCost.Text))
+            {
+                MessageBox.Show("Напишите стоимость", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(textBoxLocation.Text))
+            {
+                MessageBox.Show("Напишите Адресс", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(textBoxRooms.Text))
+            {
+                MessageBox.Show("Напишите стоимость", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(textBoxSize.Text))
+            {
+                MessageBox.Show("Напишите площадь", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                rooms.UpdElement(new Room
+                {
+                    Id = id,
+                    Name = textBoxNameRoom.Text,
+                    Price = Convert.ToDouble(textBoxCost.Text),
+                    Location = TextBoxLocation.Text,
+                    Rooms = Convert.ToInt32(TextBoxRooms.Text),
+                    Size = Convert.ToDouble(TextBoxSize.Text)
+                });
+                MessageBox.Show("Услуга успешно отредактирована!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Введите корректные значения", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            LoadData();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 1)
+            {
+                if (MessageBox.Show("Удалить запись", "Вопрос", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+                    try
+                    {
+                        rooms.DelElement(id);
+                        MessageBox.Show("Услуга успешно удалена!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    LoadData();
+                }
+            }
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+            if (dataGridView1.SelectedRows.Count == 1)
+            {
+                try
+                {
+                   Room view = rooms.GetElement(id);
+                    textBoxCost.Text = Convert.ToString(view.Price);
+                    textBoxName.Text = view.Name;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
--- HostelProduct/FormService.cs
+++ HostelProduct/FormService.cs
@@ -7,14 +7,144 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Unity;
 
 namespace HostelProduct
 {
     public partial class FormService : Form
     {
-        public FormService()
+        [Dependency]
+        public new IUnityContainer Container { get; set; }
+
+        private int id;
+        private readonly ServiceController service;
+
+        public FormService(ServiceController service)
         {
             InitializeComponent();
+            this.service = service;
+        }
+
+        private void FormService_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                List<Service> list = service.GetList();
+                if (list != null)
+                {
+                    dataGridView1.DataSource = list;
+                    dataGridView1.Columns[0].Visible = false;
+                    dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                    dataGridView1.Columns[1].HeaderText = "Название услуги";
+                    dataGridView1.Columns[2].HeaderText = "Стоимость услуги";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxNameService.Text))
+            {
+                MessageBox.Show("Заполните название услуги!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(textBoxCost.Text))
+            {
+                MessageBox.Show("Напишите стоимость", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                service.AddElement(new Service
+                {
+                    NameService = textBoxNameService.Text,
+                    CostService = Convert.ToDouble(textBoxCost.Text)
+                });
+                MessageBox.Show("Услуга успешно добавлена!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Введите корректные значения", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            LoadData();
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxNameService.Text))
+            {
+                MessageBox.Show("Заполните название услуги!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(textBoxCost.Text))
+            {
+                MessageBox.Show("Напишите стоимость!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                service.UpdElement(new Service
+                {
+                    Id = id,
+                    NameService = textBoxNameService.Text,
+                    CostService = Convert.ToDouble(textBoxCost.Text)
+                });
+                MessageBox.Show("Услуга успешно отредактирована!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Введите корректные значения", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            LoadData();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 1)
+            {
+                if (MessageBox.Show("Удалить запись", "Вопрос", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+                    try
+                    {
+                        service.DelElement(id);
+                        MessageBox.Show("Услуга успешно удалена!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    LoadData();
+                }
+            }
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+            if (dataGridView1.SelectedRows.Count == 1)
+            {
+                try
+                {
+                    Service view = service.GetElementService(id);
+                    textBoxCost.Text = Convert.ToString(view.CostService);
+                    textBoxNameService.Text = view.NameService;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
